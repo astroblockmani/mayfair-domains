f2e2649be6c92ce64ffd953eb69061655ee6a0e3
FManiR <18592101+astroblockmani@users.noreply.github.com>
2022-05-22 02:58:19 +0100

    Added .env details

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,10 +16,8 @@ module.exports = {
   solidity: "0.8.10",
   networks: {
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/yijqU_5HZzqTp_OJNrpOEM5BjXg1O_rA",
-      accounts: [
-        "20b091a7132957f51ab51d02943cb559e80da6413a24bb83ba42e1bc57b2f19b",
-      ],
+      url: process.env.PROD_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
